cdc83fe211253620e9520e87568a155c7322375b
sub123 <sbhm94@gmail.com>
2018-02-17 12:10:38 +0530

    refactor

--- greyscale.py
+++ greyscale.py
@@ -3,22 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+class grey():
 
-def brg2gray(pixel):
+	def brg2gray(self,pixel):
 	#because opencv uses brg
-	return (math.floor(0.114*pixel[0]+0.299*pixel[1]+0.587*pixel[2]))
+		return (math.floor(0.114*pixel[0]+0.299*pixel[1]+0.587*pixel[2]))
 
-img = cv.imread('test.jpg',cv.IMREAD_COLOR)
-cv.imshow("original",img)
+	def convert_to_grey(self,img):
+		width = img.shape[0]
+		height = img.shape[1];
+		gray = np.zeros((width+2,height+2))
 
-width = img.shape[0]
-height = img.shape[1];
-grey = np.zeros((width,height))
-
-for i in range(0,width):
-	for j in range(0,height):
-		grey[i][j] = brg2gray(img[i][j])
+		for i in range(0,width):
+			for j in range(0,height):
+				gray[i][j] = self.brg2gray(img[i][j])
 		#print(grey[i][j])
+		return gray
 
-plt.imshow(grey,cmap='gray',interpolation='bicubic')
-plt.show()
+#img = cv.imread('test.jpg',cv.IMREAD_COLOR)
+#to_grey=grey()
+#gray=to_grey.convert_to_grey(img)
+#plt.imshow(gray,cmap='gray',interpolation='bicubic')
+#plt.show()
--- sobel.py
+++ sobel.py
@@ -2,46 +2,36 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import greyscale
 
-kernelX = [[-1,0,1],[-2,0,2],[-1,0,1]]
-kernelY = [[-1,-2,-1],[0,0,0],[1,2,1]]
+class sobel():
+
+	kernelX = [[-1,0,1],[-2,0,2],[-1,0,1]]
+	kernelY = [[-1,-2,-1],[0,0,0],[1,2,1]]
 
 
-def brg2gray(pixel):
-	#because opencv uses brg
-	return (math.floor(0.114*pixel[0]+0.299*pixel[1]+0.587*pixel[2]))
+	threshold=10
 
-img = cv.imread('test.jpg',cv.IMREAD_COLOR)
-cv.imshow("original",img)
+	def get_sobel(self,grey,width,height):
+		sobel = np.zeros((width,height))
+		for i in range(1,width):
+			for j in range(1,height):
+				x=self.kernelX[0][0]*grey[i-1][j-1]+self.kernelX[0][1]*grey[i-1][j]+self.kernelX[0][2]*grey[i-1][j+1]+self.kernelX[1][0]*grey[i][j-1]+self.kernelX[1][1]*grey[i][j]+self.kernelX[1][2]*grey[i][j+1]+self.kernelX[2][0]*grey[i+1][j-1]+self.kernelX[2][1]*grey[i+1][j]+self.kernelX[2][2]*grey[i+1][j+1]
+				y=self.kernelY[0][0]*grey[i-1][j-1]+self.kernelY[0][1]*grey[i-1][j]+self.kernelY[0][2]*grey[i-1][j+1]+self.kernelY[1][0]*grey[i][j-1]+self.kernelY[1][1]*grey[i][j]+self.kernelY[1][2]*grey[i][j+1]+self.kernelY[2][0]*grey[i+1][j-1]+self.kernelY[2][1]*grey[i+1][j]+self.kernelY[2][2]*grey[i+1][j+1]
+				magnitude = (int)(math.sqrt(x*x+y*y))
+				if(magnitude<0):
+					magnitude=0
+				elif magnitude>255:
+					magnitude=255
+				if magnitude<self.threshold:
+					magnitude=0
+				sobel[i][j]=magnitude
+		return sobel
 
-width = img.shape[0]
-height = img.shape[1];
-grey = np.zeros((width+2,height+2))
-
-for i in range(0,width):
-	for j in range(0,height):
-		grey[i][j] = brg2gray(img[i][j])
-		#print(grey[i][j])
-
-plt.imshow(grey,cmap='gray',interpolation='bicubic')
-plt.show()
-
-threshold=10
-
-def sobel():
-	sobel = np.zeros((width,height))
-	for i in range(1,width):
-		for j in range(1,height):
-			x=kernelX[0][0]*grey[i-1][j-1]+kernelX[0][1]*grey[i-1][j]+kernelX[0][2]*grey[i-1][j+1]+kernelX[1][0]*grey[i][j-1]+kernelX[1][1]*grey[i][j]+kernelX[1][2]*grey[i][j+1]+kernelX[2][0]*grey[i+1][j-1]+kernelX[2][1]*grey[i+1][j]+kernelX[2][2]*grey[i+1][j+1]
-			y=kernelY[0][0]*grey[i-1][j-1]+kernelY[0][1]*grey[i-1][j]+kernelY[0][2]*grey[i-1][j+1]+kernelY[1][0]*grey[i][j-1]+kernelY[1][1]*grey[i][j]+kernelY[1][2]*grey[i][j+1]+kernelY[2][0]*grey[i+1][j-1]+kernelY[2][1]*grey[i+1][j]+kernelY[2][2]*grey[i+1][j+1]
-			magnitude = (int)(math.sqrt(x*x+y*y))
-			if(magnitude<0):
-				magnitude=0
-			elif magnitude>255:
-				magnitude=255
-			if magnitude<threshold:
-				magnitude=0
-			sobel[i][j]=magnitude
-	plt.imshow(sobel,cmap='gray',interpolation='bicubic')
-	plt.show()
-sobel()
+#img = cv.imread('test.jpg',cv.IMREAD_COLOR)
+#to_grey=greyscale.grey()
+#gray=to_grey.convert_to_grey(img)
+#to_sobel=sobel()
+#energy_map=to_sobel.get_sobel(gray,img.shape[0],img.shape[1])
+#plt.imshow(energy_map,cmap='gray',interpolation='bicubic')
+#plt.show()
